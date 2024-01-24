ce287028877bee11020802c5bb36f964250e535b
patportugal <157312773+patportugal@users.noreply.github.com>
2024-01-24 18:21:09 +0100

    not working JS search engine_ Fixed

--- app/script.js
+++ app/script.js
@@ -21,16 +21,19 @@ function displayTemperature(response) {
   getForecast(response.data.city);
 }
 
-function searchCity(city) {
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  if (searchInputElement.value === "") {
+    searchInputElement = "Cologne";
+  }
+
   let apiKey = `753ef61a4c9704b0boa8ce19973atca6`;
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=753ef61a4c9704b0boa8ce19973atca6&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
-}
-
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
 }
 
 function formatDate(date) {
@@ -117,4 +120,3 @@ function displayForecast(response) {
 }
 
 getForecast("cologne");
-searchCity("cologne");
